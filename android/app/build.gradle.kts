5e562b631946909ffb3d9a07a41d64ff65fb7ea3
Mohimem <sulimangzllal@alMacBook-Pro-mohimen-tantoush.local>
2025-08-20 04:03:56 +0200

    fix issues for android folder

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,15 +6,15 @@ plugins {
     id("com.google.gms.google-services")
 }
 
+
 dependencies {
-  // Import the Firebase BoM
-  implementation(platform("com.google.firebase:firebase-bom:34.0.0"))
-  coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:34.0.0"))
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 
-
-  // TODO: Add the dependencies for Firebase products you want to use
-  // When using the BoM, don't specify versions in Firebase dependencies
-  // https://firebase.google.com/docs/android/setup#available-libraries
+    // TODO: Add the dependencies for Firebase products you want to use
+    // When using the BoM, don't specify versions in Firebase dependencies
+    // https://firebase.google.com/docs/android/setup#available-libraries
 }
 
 android {
@@ -34,11 +34,8 @@ android {
 
     defaultConfig {
         multiDexEnabled = true
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.resqtracksos.app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23 // <-- This is the fix
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
